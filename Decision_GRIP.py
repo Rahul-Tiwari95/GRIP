a78c3ab89f76f14224119c2f5bc1b183dba89c54
Rahul-Tiwari95 <66791133+Rahul-Tiwari95@users.noreply.github.com>
2020-08-15 13:51:38 +0530

    Add files via upload

---
+++ Decision_GRIP.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 12:35:00 2020
+
+@author: rahul
+"""
+
+import pandas as pd
+data = pd.read_csv("E:\\Data Science\\Data Sheet\\iris.csv")
+data.head()
+data['Species'].unique()
+data.Species.value_counts()
+colnames = list(data.columns)
+predictors = colnames[1:5]
+target = colnames[5]
+
+# Splitting data into training and testing data set
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+train,test = train_test_split(data,test_size = 0.2)
+
+from sklearn.tree import  DecisionTreeClassifier
+
+model = DecisionTreeClassifier(criterion = 'entropy')
+model.fit(train[predictors],train[target])
+
+preds = model.predict(test[predictors])
+pd.Series(preds).value_counts()
+pd.crosstab(test[target],preds)
+
+# Accuracy = train
+np.mean(train.Species == model.predict(train[predictors]))
+
+# Accuracy = Test
+np.mean(preds==test.Species) 
